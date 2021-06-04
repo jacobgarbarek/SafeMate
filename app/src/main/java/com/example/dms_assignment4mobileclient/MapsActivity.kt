518d9ef12552392fbd120ecafeec772fd077f08b
Jacob Garbarek <xnf3679@autuni.ac.nz>
2021-06-04 18:17:10 +1200

    Current location feature

--- app/src/main/java/com/example/dms_assignment4mobileclient/MapsActivity.kt
+++ app/src/main/java/com/example/dms_assignment4mobileclient/MapsActivity.kt
@@ -1,20 +1,30 @@
 package com.example.dms_assignment4mobileclient
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
+import com.example.dms_assignment4mobileclient.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.example.dms_assignment4mobileclient.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.LatLngBounds
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMyLocationButtonClickListener,
+    OnMyLocationClickListener, OnMapReadyCallback, OnRequestPermissionsResultCallback {
 
+    //private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var permissionDenied = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +48,113 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        mMap = googleMap ?: return
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+        enableMyLocation()
+        googleMap.setOnMapLoadedCallback {
+            val bounds = LatLngBounds.builder()
+        }
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun enableMyLocation() {
+        if (!::mMap.isInitialized) return
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            requestPermission(
+                this, LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun requestPermission(activity: MapsActivity, requestId: Int, permission: String) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            // Display a dialog with rationale.
+            AlertDialog.Builder(this)
+                .setTitle("Permission Request")
+                .setMessage("Need access to GPS")
+                .setPositiveButton("Allow"){dialog, which ->
+                    ActivityCompat.requestPermissions(
+                        activity!!,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        requestId
+                    )
+                }
+                .create()
+                .show()
+        } else {
+            // Location permission has not been granted yet, request it.
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(permission),
+                requestId
+            )
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+        if (isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume
+            permissionDenied = true
+        }
+    }
+
+    private fun isPermissionGranted(
+        grantPermissions: Array<String>, grantResults: IntArray,
+        permission: String
+    ): Boolean {
+        for (i in grantPermissions.indices) {
+            if (permission == grantPermissions[i]) {
+                return grantResults[i] == PackageManager.PERMISSION_GRANTED
+            }
+        }
+        return false
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            permissionDenied = false
+            AlertDialog.Builder(this)
+                .setTitle("Permission Request")
+                .setMessage("Need access to GPS")
+                .setPositiveButton("Allow"){dialog, which ->
+                    ActivityCompat.requestPermissions(
+                        this!!,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        LOCATION_PERMISSION_REQUEST_CODE
+                    )
+                }
+                .create()
+                .show()
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1;
     }
 }
